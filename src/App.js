c25518a3b2820b7a9de5108c988d80dd14525ac3
Sahaj_Bhadja <sahajbhadja10@gmail.com>
2024-02-29 11:20:42 +0530

    frontend basics

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Invoices from "./scenes/invoices";
+import Contacts from "./scenes/contacts";
+import Bar from "./scenes/bar";
+import Form from "./scenes/form";
+import Line from "./scenes/line";
+import Pie from "./scenes/pie";
+import FAQ from "./scenes/faq";
+import Geography from "./scenes/geography";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { ColorModeContext, useMode } from "./theme";
+import Calendar from "./scenes/calendar/calendar";
+import Login from "./Sign in/Login";
+import SignUp from "./Sign in/Signup";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  // Check if the current route is "/" or "/signup"
+  const isHomePage =
+    window.location.pathname === "/" || window.location.pathname === "/signup";
+
+  // If it's the home page, don't display Topbar and Sidebar
+  const shouldDisplayTopbarAndSidebar = !isHomePage;
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route
+          path="/*"
+          element={
+            shouldDisplayTopbarAndSidebar && (
+              <>
+                <ColorModeContext.Provider value={colorMode}>
+                  <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <div className="app">
+                      {shouldDisplayTopbarAndSidebar && (
+                        <Sidebar isSidebar={isSidebar} />
+                      )}
+                      <main className="content">
+                        {shouldDisplayTopbarAndSidebar && (
+                          <Topbar setIsSidebar={setIsSidebar} />
+                        )}
+                        <Routes>
+                          <Route path="/dashboard" element={<Dashboard />} />
+                          <Route path="/team" element={<Team />} />
+                          <Route path="/contacts" element={<Contacts />} />
+                          <Route path="/invoices" element={<Invoices />} />
+                          <Route path="/form" element={<Form />} />
+                          <Route path="/bar" element={<Bar />} />
+                          <Route path="/pie" element={<Pie />} />
+                          <Route path="/line" element={<Line />} />
+                          <Route path="/faq" element={<FAQ />} />
+                          <Route path="/calendar" element={<Calendar />} />
+                          <Route path="/geography" element={<Geography />} />
+                        </Routes>
+                      </main>
+                    </div>
+                  </ThemeProvider>
+                </ColorModeContext.Provider>
+              </>
+            )
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
