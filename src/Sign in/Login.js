9dc643e2bb6f8cc03a11ecfb32abd2863ce46165
Sahaj_Bhadja <sahajbhadja10@gmail.com>
2024-02-29 12:54:05 +0530

    removed copyright

--- src/Sign in/Login.js
+++ src/Sign in/Login.js
@@ -14,24 +14,6 @@ import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
 
-function Copyright(props) {
-  return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
-    >
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
-
 
 const defaultTheme = createTheme();
 
@@ -128,7 +110,6 @@ export default function SignIn() {
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4 }} />
       </Container>
     </ThemeProvider>
   );
